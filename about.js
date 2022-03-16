92036f095b18537201c25a6bc47df297f59135ba
Altan013 <94452540+Altan013@users.noreply.github.com>
2022-03-16 18:54:36 +0400

    Add files via upload

---
+++ about.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.welcome-slider').slick({
+    dots: false,
+    infinity: true,
+    autoplay: true,
+    speed: 900,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true
+  });
+});
